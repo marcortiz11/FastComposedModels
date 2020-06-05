1473ce7108c2dcfb21ffd35b16d6b4b6a22fec0c
marcortiz11 <mrk.22.10@gmail.com>
2020-06-05 17:10:04 +0200

    Recovering deleted files from duplicated folders 'Examples' and 'Source'

--- examples/study/chain_genetic_algorithm/display_solutions.py
+++ examples/study/chain_genetic_algorithm/display_solutions.py
@@ -7,20 +7,18 @@ import os
 if __name__ == "__main__":
 
     # Information about the experiment
-    dataset = 'sota_models_cifar10-32-dev_validation'
-    experiment = 'chain_genetic_algorithm_multinode'
+    experiment = 'chain_genetic_algorithm'
     experiment_dir = os.path.join(os.environ['FCM'], 'examples', 'compute', experiment)
     meta_data_file = os.path.join(experiment_dir, 'results', 'metadata.json')
-    id = "7095599150457036"
+    id = "54099516605262"
 
     # Retrieve the results of the experiments
-    models_data_path = os.path.join(os.environ['FCM'], 'small_examples', 'models_evaluation', dataset, 'models.pkl')
+    # models_data_path = os.path.join(os.environ['FCM'], 'small_examples', 'models_evaluation', dataset, 'models_cpu.pkl')
     chain_data_path = os.path.join(experiment_dir, results_manager.get_results_by_id(meta_data_file, id))
+    dataset = results_manager.get_fieldval_by_id(meta_data_file, id, 'dataset')[0][12:-18]
 
     chain = io.read_pickle(chain_data_path)
-    models = io.read_pickle(models_data_path)
 
-    myplt.plot_accuracy_time_old(chain)
-    myplt.plot_accuracy_time_old(models)
+    myplt.plot_accuracy_parameters_time(chain, dataset+' test solution space')
     myplt.show()
 
