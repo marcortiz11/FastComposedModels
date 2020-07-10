43cc8b0944bc86c9763d912c5f36300ff98b85fe
marcortiz11 <mrk.22.10@gmail.com>
2020-07-10 21:06:45 +0200

    Unittests - Extend and Replacement operators in chains of classifiers

---
+++ Tests/test_extend_chain_operation.py
@@ -0,0 +1,121 @@
+import Source.system_builder_serializable as sb
+import Source.make_util as mutils
+import Source.io_util as io
+from Source.system_evaluator import evaluate
+from Source.system_evaluator_utils import  pretty_print
+from Source.genetic_algorithm.operations_mutation import extend_merged_chain
+
+import unittest
+import os
+import numpy as np
+import math
+
+
+class ExtendChain(unittest.TestCase):
+
+    def __create_ensemble(self):
+        ensemble = sb.SystemBuilder()
+        c_id = 'ResNet18'
+        c_file = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers', 'sota_models_cifar10-32-dev_validation',
+                              'V001_ResNet18_ref_0.pkl')
+        classifier = mutils.make_classifier(c_id, c_file)
+        ensemble.add_classifier(classifier)
+        ensemble.set_start(c_id)
+        return ensemble
+
+    def test_threshold_0(self):
+        chain = self.__create_ensemble()
+        c_id_extend = 'DenseNet-161'
+        c_file_extend = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers',
+                                     'sota_models_cifar10-32-dev_validation', 'V001_DenseNet161_ref_0.pkl')
+        extend_merged_chain(chain, 'ResNet18', c_id_extend, 0, c_file_extend)
+        R = evaluate(chain, chain.get_start())
+        acc_chain = R.test['system'].accuracy
+        time_chain = R.test['system'].time
+        ops_chain = R.test['system'].ops
+        params_chain = R.test['system'].params
+
+
+        # Accuracy classifier by hand
+        dict_classifier = io.read_pickle(os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers', 'sota_models_cifar10-32-dev_validation',
+                              'V001_ResNet18_ref_0.pkl'))
+        acc_net0 = np.sum(np.argmax(dict_classifier['test']['logits'], 1) == dict_classifier['test']['gt']) / len(
+            dict_classifier['test']['gt'])
+        time_net0 = dict_classifier['metrics']['time']
+        params_net0 = dict_classifier['metrics']['params']
+        ops_net0 = dict_classifier['metrics']['ops']
+
+        # Accuracy classifier by hand
+        dict_classifier = io.read_pickle(c_file_extend)
+        params_net1 = dict_classifier['metrics']['params']
+
+
+        correct = acc_chain == acc_net0 and \
+                    math.isclose(time_net0/128, time_chain/5e3, rel_tol=1e-09) and \
+                    ops_net0 == ops_chain/5e3 and\
+                    params_chain == params_net0 + params_net1 + 1
+
+        self.assertEqual(correct, True)
+
+    def test_threshold_1(self):
+        chain = self.__create_ensemble()
+        c_id_extend = 'DenseNet-161'
+        c_file_extend = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers',
+                                     'sota_models_cifar10-32-dev_validation', 'V001_DenseNet161_ref_0.pkl')
+        extend_merged_chain(chain, 'ResNet18', c_id_extend, 1.1, c_file_extend)
+        R = evaluate(chain, chain.get_start())
+        acc_chain = R.test['system'].accuracy
+        time_chain = R.test['system'].time
+        ops_chain = R.test['system'].ops
+        params_chain = R.test['system'].params
+
+
+        # Accuracy classifier by hand
+        dict_classifier = io.read_pickle(os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers', 'sota_models_cifar10-32-dev_validation',
+                              'V001_ResNet18_ref_0.pkl'))
+        acc_net0 = np.sum(np.argmax(dict_classifier['test']['logits'], 1) == dict_classifier['test']['gt']) / len(
+            dict_classifier['test']['gt'])
+        time_net0 = dict_classifier['metrics']['time']
+        params_net0 = dict_classifier['metrics']['params']
+        ops_net0 = dict_classifier['metrics']['ops']
+
+        # Accuracy classifier by hand
+        dict_classifier = io.read_pickle(c_file_extend)
+        acc_net1 = np.sum(np.argmax(dict_classifier['test']['logits'], 1) == dict_classifier['test']['gt']) / len(
+            dict_classifier['test']['gt'])
+        time_net1 = dict_classifier['metrics']['time']
+        params_net1 = dict_classifier['metrics']['params']
+        ops_net1 = dict_classifier['metrics']['ops']
+
+        correct = acc_chain == acc_net1 and \
+                    math.isclose(time_net0/128 + time_net1/128, time_chain/5e3) and \
+                    ops_chain/5e3 == ops_net0 + ops_net1 and \
+                    params_chain == params_net0 + params_net1 + 1
+
+        self.assertEqual(correct, True)
+
+    def test_structure(self):
+
+        chain = self.__create_ensemble()
+        c_id_extend = 'DenseNet-161'
+        c_file_extend = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers',
+                                     'sota_models_cifar10-32-dev_validation', 'V001_DenseNet161_ref_0.pkl')
+        extend_merged_chain(chain, 'ResNet18', c_id_extend, 1.1, c_file_extend)
+
+        correct_structure = True
+
+        # Check connections between components
+        correct_structure = correct_structure and \
+                            "trigger_classifier_1.1_ResNet18" == chain.get(chain.get_start()).component_id
+
+        correct_structure = correct_structure and \
+                            len(chain.get("trigger_classifier_1.1_ResNet18").component_ids) == 1
+
+        correct_structure = correct_structure and \
+                            chain.get("trigger_classifier_1.1_ResNet18").component_ids[0] == c_id_extend
+
+        self.assertEqual(correct_structure, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Tests/test_replace_classifier_chain_operation.py
@@ -0,0 +1,75 @@
+import Source.system_builder_serializable as sb
+import Source.make_util as mutils
+import Source.io_util as io
+from Source.system_evaluator import evaluate
+from Source.system_evaluator_utils import  pretty_print
+from Source.genetic_algorithm.operations_mutation import extend_merged_chain
+from Source.genetic_algorithm.operations_mutation import replace_classifier_merger
+
+import unittest
+import os
+import numpy as np
+import math
+
+
+def create_chain():
+    ensemble = sb.SystemBuilder()
+    c_id = 'ResNet18'
+    c_file = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers', 'sota_models_cifar10-32-dev_validation',
+                          'V001_ResNet18_ref_0.pkl')
+    classifier = mutils.make_classifier(c_id, c_file)
+    ensemble.add_classifier(classifier)
+    ensemble.set_start(c_id)
+    return ensemble
+
+
+def extend_chain(chain):
+    c_id_extend = 'DenseNet161'
+    c_file_extend = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers',
+                                 'sota_models_cifar10-32-dev_validation', 'V001_DenseNet161_ref_0.pkl')
+    extend_merged_chain(chain, 'ResNet18', c_id_extend, 0.0, c_file_extend)
+
+
+def replace(chain):
+    c_file_replace = os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers',
+                                  'sota_models_cifar10-32-dev_validation', 'V001_DenseNet169_ref_0.pkl')
+    replace_classifier_merger(chain, 'ResNet18', 'DenseNet169', c_file=c_file_replace)
+
+
+class ReplaceClassifier(unittest.TestCase):
+
+    def test_accuracy(self):
+        chain = create_chain()
+        extend_chain(chain)
+        replace(chain)
+        R = evaluate(chain, chain.get_start())
+
+        # Accuracy classifier by hand
+        dict_classifier = io.read_pickle(
+            os.path.join(os.environ['FCM'], 'Definitions', 'Classifiers', 'sota_models_cifar10-32-dev_validation',
+                         'V001_DenseNet169_ref_0.pkl'))
+        acc_net0 = np.sum(np.argmax(dict_classifier['test']['logits'], 1) == dict_classifier['test']['gt']) / len(
+            dict_classifier['test']['gt'])
+
+        self.assertEqual(acc_net0, R.test['system'].accuracy)
+
+    def test_structure_connections_chain(self):
+        chain = create_chain()
+        extend_chain(chain)
+        replace(chain)
+
+        correct = chain.get(chain.get_start()).component_id == 'trigger_classifier_0.0_DenseNet169' and \
+                chain.get('trigger_classifier_0.0_DenseNet169').component_ids[0] == 'DenseNet161' and \
+                len(chain.get('trigger_classifier_0.0_DenseNet169').component_ids) == 1
+        self.assertTrue(correct)
+
+    def test_structure_check_start(self):
+        chain = create_chain()
+        extend_chain(chain)
+        replace(chain)
+
+        self.assertTrue(chain.get_start() == 'DenseNet169')
+
+
+if __name__ == '__main__':
+    unittest.main()
