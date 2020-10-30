2bd696cad1d392eadcba43596b7db13404e02bb3
marcortiz11 <mrk.22.10@gmail.com>
2020-10-30 14:37:40 +0100

    [EARN NGSA2] Added the crowding distance as a secondary elitism selection criterion

--- Examples/compute/bagging_boosting_of_chains_GA/main_NGSA2.py
+++ Examples/compute/bagging_boosting_of_chains_GA/main_NGSA2.py
@@ -1,15 +1,17 @@
 """
-EARN following GP genetic operations format
+EARN following GP genetic operations format under NGSA2 elitism selection
 """
 
 from Examples.compute.bagging_boosting_of_chains_GA.main import *
+from Source.genetic_algorithm.fitting_functions import f1_3objective_acc_time_param as f
 del globals()["mutation_operation"]
 del globals()["generate_offspring"]
+del globals()["crossover_operation_v2"]
 import Examples.compute.bagging_boosting_of_chains_GA.main as main
 import numpy as np
 
 
-def mutation_operation(P):
+def mutation_operation(P: list) -> list:
 
     p = P[random.randint(0, len(P) - 1)]
     new_p = p.copy()
@@ -56,7 +58,40 @@ def mutation_operation(P):
     return [new_p]
 
 
-def generate_offspring(P, fit_vals, o=None):
+def crossover_operation_v2(P:list, rank_dist:np.ndarray) -> list:
+
+    assert len(P) > 0 and rank_dist.shape[1] == 2
+
+    # Tournment selection of parent ensembles
+    rank = rank_dist[:, 0]
+    dist = rank_dist[:, 1]
+    K = min(rank.shape[0]//2, main.args.k)
+
+    ai = bi = 0
+    while ai == bi:
+        ai = selection.tournament_selection(rank, K, dist)  # Select according to rank and crowding distance
+        bi = selection.tournament_selection(rank, K, dist)
+    a = P[ai]
+    b = P[bi]
+
+    offspring = []
+
+    # If two parents are chains merge them
+    if len(a.get_message().merger) == 0 and len(b.get_message().merger) == 0:
+        o = ob.merge_two_chains(a, b)
+        offspring.append(o)
+
+    # Single-Point Crossover of two Ensembles
+    classifiersA = a.get_message().classifier
+    classifiersB = b.get_message().classifier
+    pointA = classifiersA[random.randint(0, len(classifiersA) - 1)].id
+    pointB = classifiersB[random.randint(0, len(classifiersB) - 1)].id
+    offspring += ob.singlepoint_crossover(a, b, pointA, pointB)
+
+    return offspring
+
+
+def generate_offspring(P:list, rank_dist: np.ndarray, o=None) -> list:
 
     if o is None:
         o = main.args.offspring
@@ -69,7 +104,7 @@ def generate_offspring(P, fit_vals, o=None):
             for offspring in mutation_operation(P):
                 offspring_dict[offspring.get_sysid()] = offspring
         if main.args.pc > r:
-            for offspring in crossover_operation_v2(P, fit_vals):
+            for offspring in crossover_operation_v2(P, rank_dist):
                 offspring_dict[offspring.get_sysid()] = offspring
 
     return [offspring for key, offspring in offspring_dict.items()]
@@ -80,13 +115,14 @@ def argument_parse(argv):
                                      description="Genetic algorithm for finding the best chain under environment constraints.")
     parser.add_argument("--dataset", default="sota_models_cifar10-32-dev_validation", help="Datasets to evaluate d1 d2 d3, default=*")
     # Search-space params
-    parser.add_argument("--population", default=1000, type=int, help="Population at each generation")
+    parser.add_argument("--population", default=500, type=int, help="Population at each generation")
     parser.add_argument("--offspring", default=100, type=int, help="Children generated at each generation")
-    parser.add_argument("--iterations", default=200, type=int, help="Number of iterations before finishing algorithm")
+    parser.add_argument("--iterations", default=50, type=int, help="Number of iterations before finishing algorithm")
     parser.add_argument("--step_th", default=0.1, type=float, help="Quant modification in threshold")
-    parser.add_argument("--pm", default=0.9, type=float, help="Probability of mutation")
-    parser.add_argument("--pc", default=0.1, type=float, help="Probability of crossing/breeding")
+    parser.add_argument("--pm", default=0.8, type=float, help="Probability of mutation")
+    parser.add_argument("--pc", default=0.2, type=float, help="Probability of crossing/breeding")
     parser.add_argument("--k", default=10, type=int, help="Tournament size")
+    parser.add_argument("--a", nargs='+', default=[1, 1, 1], type=float, help="Fitting function's weight")
     # Execution parameters
     parser.add_argument("--plot", default=0, type=int, help="Plot the ensembles generated every generation")
     parser.add_argument("--cores", default=0, type=int, help="Parallel evaluation of the ensembles")
@@ -97,16 +133,25 @@ def argument_parse(argv):
     return parser.parse_args(argv)
 
 
-def dominates(r1, r2):
-    d = r1.val["system"].accuracy >= r2.val["system"].accuracy and \
-        r1.val["system"].time <= r2.val["system"].time and \
-        r1.val["system"].params <= r2.val["system"].params
-    return d
+def get_rank_crowding_dist(R: list, limits: dict) -> np.ndarray:
+    rank = fast_non_dominated_sort(R)
+    crowd_dist = np.zeros(rank.shape)
+    for ri in np.unique(rank):
+        positions = np.where(rank == ri)[0]
+        Ri = [R[i] for i in positions]
+        normalized_objectives = f(Ri, main.args.a, limits)
+        crowd_dist[positions] = compute_crowding_distance(normalized_objectives)
+    return np.column_stack((rank, -crowd_dist))
 
 
-def fast_non_dominated_sort(R):
+def fast_non_dominated_sort(R: list) -> np.ndarray:
+
+    dominates = lambda r1, r2: r1.val["system"].accuracy >= r2.val["system"].accuracy and\
+                               r1.val["system"].time <= r2.val["system"].time and \
+                               r1.val["system"].params <= r2.val["system"].params
+
     rank = -np.ones(len(R), dtype=np.int)  # Rank of the solution
-    N = [-1] * len(R)  # Dominant solutions
+    N = -np.ones(rank.shape)  # Dominant solutions
     S = []  # Dominated solutions
     F = set()  # Current front
 
@@ -135,10 +180,20 @@ def fast_non_dominated_sort(R):
                 if N[qi] == 0:
                     rank[qi] = i+1
                     Q.add(qi)
-        i+=1
+        i += 1
         F = Q
 
     return rank
+
+
+def compute_crowding_distance(obj: np.ndarray) -> np.ndarray:
+    D = np.zeros(obj.shape[0])
+    for j in range(obj.shape[1]):
+        obj_id_sorted = np.argsort(obj[:, j])
+        D[obj_id_sorted[0]] = D[obj_id_sorted[-1]] = float("inf")
+        for i in range(1, obj.shape[0] - 1):
+            D[obj_id_sorted[i]] += obj[obj_id_sorted[i+1], j] - obj[obj_id_sorted[i-1], j]
+    return D
 
 
 if __name__ == "__main__":
@@ -153,8 +208,6 @@ if __name__ == "__main__":
     # Initial population
     P = generate_initial_population()
     R = evaluate_population(P)
-    rank = fast_non_dominated_sort(R)
-    P_all = []
     individuals_fitness_per_generation = []
 
     # Evaluation Results Dictionary
@@ -162,6 +215,10 @@ if __name__ == "__main__":
     R_dict_old = {}
     R_dict_models = dict(zip([p.get_sysid() for p in P], R))
     R_dict_old.update(R_dict_models)
+    limits = fit_fun.make_limits_dict()
+    fit_fun.update_limit_dict(limits, R_dict_models, phase="val")
+
+    rank_dist = get_rank_crowding_dist(R, limits)  # Numpy array
 
     # Start the loop over generations
     iteration = 0
@@ -170,18 +227,18 @@ if __name__ == "__main__":
         start = time.time()
 
         # Generate offspring and evaluate
-        P_offspring = generate_offspring(P, -rank)
+        P_offspring = generate_offspring(P, rank_dist)
         R_offspring = evaluate_population(P_offspring)
 
         # Selection
         P_generation = P + P_offspring
         R_generation = R + R_offspring
-        rank_generation = fast_non_dominated_sort(R_generation)
+        rank_dist_generation = get_rank_crowding_dist(R_generation, limits)
 
-        selected = np.argsort(rank_generation)[:main.args.population]
+        selected = np.lexsort(rank_dist_generation.T[::-1])[:main.args.population]
         P = [P_generation[i] for i in selected]
         R = [R_generation[i] for i in selected]
-        rank = rank_generation[selected]  # Numpy array
+        rank_dist = rank_dist_generation[selected]
 
         # Plotting population
         R_dict_old.update(R_dict)
@@ -193,7 +250,7 @@ if __name__ == "__main__":
 
         # Save which individuals alive every iteration
         ids = [p.get_sysid() for p in P]
-        individuals_fitness_per_generation += [(ids, rank)]
+        individuals_fitness_per_generation += [(ids, rank_dist)]
 
         # Info about current generation
         print("Generation %d" % iteration)
